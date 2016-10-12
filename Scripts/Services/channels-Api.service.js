641562ac7afd30c366a29536dc27ad11da6964bb
AndreeaFC <andreea_vhr@yahoo.com>
2016-10-12 13:44:33 +0200

    init commit

---
+++ Scripts/Services/channels-Api.service.js
@@ -0,0 +1,63 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("mainModule")
+    .service("channelsApi", [
+        "$http",
+        "$q",
+        function ($http, $q) {
+            var api = "http://dummyapi.kodalagom.se/api";
+            var channels = api + "/channels";
+
+            this.getChannels = function () {
+                var deferred = $q.defer();
+
+                $http.get(channels)
+                  .then(function (response) {
+                      deferred.resolve(response.data);
+                  }, function (response) {
+                      deferred.resolve([]);
+                  });
+
+                return deferred.promise;
+            };
+
+            this.addChannel = function (newChannel) {
+                var deferred = $q.defer();
+
+                $http.channel(channels, newChannel)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    }, function (response) {
+                        deferred.resolve([]);
+                    });
+
+                return deferred.promise;
+            };
+
+            this.updateChannel = function (updateMessage) {
+                var deferred = $q.defer();
+
+                $http.put(message + "/" + updateMessage.id, updateMessage)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    }, function (response) {
+                        deferred.resolve([]);
+                    });
+
+                return deferred.promise;
+            };
+
+            this.deleteMessage = function (id) {
+                var deferred = $q.defer();
+
+                $http.delete(message + "/" + id)
+                    .then(function (response) {
+                        deferred.resolve();
+                    }, function (response) {
+                        deferred.resolve();
+                    });
+
+                return deferred.promise;
+            };            
+        }
+    ])
---
+++ Scripts/Services/messages-Api.service.js
@@ -0,0 +1,65 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("mainModule")
+    .service("messagesApi", [
+        "$http",
+        "$q",
+        function ($http, $q) {
+            var api = "http://dummyapi.kodalagom.se/api";
+            var messages = api + "/messages";
+
+            this.getMessages = function () {
+                var deferred = $q.defer();
+
+                $http.get(messages)
+                  .then(function (response) {
+                      deferred.resolve(response.data);
+                  }, function (response) {
+                      deferred.resolve([]);
+                  });
+
+                return deferred.promise;
+            };
+
+            this.addMessage = function (newMessage) {
+                var deferred = $q.defer();
+
+                $http.message(messages, newMessage)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    }, function (response) {
+                        deferred.resolve([]);
+                    });
+
+                return deferred.promise;
+            };
+
+            this.updateMessage = function (updateMessage) {
+                var deferred = $q.defer();
+
+                $http.put(message + "/" + updateMessage.id, updateMessage)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    }, function (response) {
+                        deferred.resolve([]);
+                    });
+
+                return deferred.promise;
+            };
+
+            this.deleteMessage = function (id) {
+                var deferred = $q.defer();
+
+                $http.delete(message + "/" + id)
+                    .then(function (response) {
+                        deferred.resolve();
+                    }, function (response) {
+                        deferred.resolve();
+                    });
+
+                return deferred.promise;
+            };
+
+
+        }
+    ])
